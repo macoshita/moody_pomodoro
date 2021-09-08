5b7af80f57aa9279a61e37bf73c2fbb2de34fa09
macoshita <macoshita@users.noreply.github.com>
2021-09-08 13:34:30 +0900

    Add google fonts CrimsonText

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:moody_pomodoro/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(ProviderScope(child: App()));
 }
